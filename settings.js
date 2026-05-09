466db3bb5f70d5c248584f86a79a0162d5f0a5cf
kendrid-05 <kellyjesh@gmail.com>
2026-05-10 01:03:24 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Kendrid kelly
+  botOwner: 'kendrid kelly', // your name
   ownerNumber: '256709600806', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
